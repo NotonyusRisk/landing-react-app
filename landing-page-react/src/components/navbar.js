70cd050901bef4ec3aa79a1df9c0e405349e5dd8
Antonio <antoniojrudass@gmail.com>
2024-04-15 14:34:17 -0500

    update

--- landing-page-react/src/components/navbar.js
+++ landing-page-react/src/components/navbar.js
@@ -4,7 +4,17 @@ export function Navbar() {
             <h1>Hello world</h1>
         </div>
         <div className='navbar-options'>
-
+            <ul>
+                <li>
+                    <a>Inicio</a>
+                </li>
+                <li>
+                    <a>About Us</a>
+                </li>
+                <li>
+                    <a>Contact Us</a>
+                </li>
+            </ul>
         </div>
     </navbar>
 }
